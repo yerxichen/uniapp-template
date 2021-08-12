f9482bae946cb6a6095bac29d1cb98be363edd76
879908188@qq.com <Mhp2g007>
2021-08-12 16:18:59 +0800

    去除test环境

--- common/env/constants.js
+++ common/env/constants.js
@@ -1,18 +1,14 @@
 import {common} from "./common";
 import {dev} from './dev'
-import {test} from './test'
 import {prod} from './prod'
 
 function envConstants() {
     console.log(process.env.NODE_ENV);
-    if (process.env.VUE_APP_ENV === 'test') {
-        console.log('测试环境')
-        Object.assign(common, test)
-    } else if (process.env.VUE_APP_ENV === 'prod') {
+    if (process.env.NODE_ENV === 'production') {
         console.log('生产环境')
         Object.assign(common, prod)
     } else {
-        console.log('本地环境')
+        console.log('开发环境')
         Object.assign(common, dev)
     }
     return common
--- common/env/test.js
+++
@@ -1,4 +0,0 @@
-//测试环境专属配置
-export const test = {
-    baseUrl: 'http://wwg-test.sjqsoft.com:10080/sz-scwgt'
-}
