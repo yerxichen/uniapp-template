28bf5793fa2e2dadec71781397fa478139ad4421
yer <879908188@qq.com>
2021-11-11 16:51:28 +0800

    更改模板，增加mock数据

--- common/env/constants.js
+++ common/env/constants.js
@@ -2,6 +2,8 @@ import common from "./common";
 import dev from './dev'
 import prod from './prod'
 import userMock from '@/mock/index.js'
+import Vconsole from 'vconsole'
+
 const constants = function envConstants() {
     console.log(process.env.NODE_ENV);
     if (process.env.NODE_ENV === 'production') {
@@ -14,6 +16,8 @@ const constants = function envConstants() {
     }
     // 开启mock数据
     if(common.useMock) userMock()
+    // 开启vconsole
+    if(common.useVconsole) new Vconsole()
     return common
 }
 
--- common/env/dev.js
+++ common/env/dev.js
@@ -1,5 +1,6 @@
 //开发环境专属配置
 export default {
     baseUrl: 'http://mockjs',
-    useMock: true
+    useMock: true, // 开启mock
+    useVconsole: true // 开启vconsole
 }
