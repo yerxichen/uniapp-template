91f12b781ed01f7b61bce551784fd151867f1bfa
yer <879908188@qq.com>
2021-11-16 14:43:56 +0800

    更改模板，增加mock数据

--- mock/index.js
+++ mock/index.js
@@ -1,19 +1,20 @@
 import Mock from 'mockjs'
 import address from './address.js'
 import dev from '@/common/env/dev.js'
+
 const mocks = [
-	...address
+    ...address
 ]
 
-const usesMock = function (){
-	for (const i of mocks) {
-		Mock.mock(dev.baseUrl + i.url, i.type || 'get', {
-			success: true,
-			code: 0,
-			result: i.response.data,
-			message: '成功'
-		})
-	}
+const usesMock = function () {
+    for (const i of mocks) {
+        Mock.mock(dev.baseUrl + i.url, i.type || 'get', {
+            success: true,
+            code: 0,
+            result: i.response.data,
+            message: '成功'
+        })
+    }
 }
 
 export default usesMock
